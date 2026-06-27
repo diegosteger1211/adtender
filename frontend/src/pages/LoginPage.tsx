6ea6a783a46f6817efd64643589928ffdb802d1c
diegosteger1211 <141810424+diegosteger1211@users.noreply.github.com>
2026-06-27 15:59:20 +0200

    Replace PNG logo with transparent SVG version

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -63,7 +63,7 @@ export default function LoginPage() {
         />
         {/* Logo */}
         <div className="relative z-10">
-          <img src="/adtender-logo.png" alt="adtender" className="h-24 w-auto object-contain" style={{ mixBlendMode: 'screen' }} />
+          <img src="/adtender-logo.svg" alt="adtender" className="h-24 w-auto object-contain" />
         </div>
 
         {/* Center content */}
